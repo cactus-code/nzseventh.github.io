c0667a3e8879a0a9b633376dec693c83a2b31815
cactus-code <cactus-code@users.noreply.github.com>
2017-08-25 12:28:20 +1200

    Fixed mobile redirect

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
   }
   if(isMobile()){
-    header("Location: http://www.nzseventh.tk/mobile/");
+    header("Location: http://www.nzseventh.tk/mobile/index.html");
   }
   ?>
   <base href="http://www.nzseventh.tk/">
